9f22c235f05664e8c347b91bd332da8b62ba606a
unknown <clementugomaliki@gmail.com>
2026-06-30 19:19:14 +0100

    set up the react folder, removed all default css and set up tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h2 className='text-red-600'>Confirmation of page setup for react</h2>
+    </div>
+  )
+}
+
+export default App
